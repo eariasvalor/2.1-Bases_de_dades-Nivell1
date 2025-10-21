c984da88fb1b536493424d4415ee0c729daf96d0
Esther Arias <eariasvalor@gmail.com>
2025-10-21 12:19:19 +0200

    feat: add table brand

--- src/culdampolla_optics.sql
+++ src/culdampolla_optics.sql
@@ -8,9 +8,18 @@ telephone VARCHAR(20),
 fax VARCHAR(20),
 NIF VARCHAR(10)
 );
+
+CREATE TABLE brand (
+id INT AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(100) NOT NULL,
+id_provider INT NOT NULL,
+FOREIGN KEY (id_provider) REFERENCES provider(id)
+ON UPDATE CASCADE ON DELETE RESTRICT
+);
+
 CREATE TABLE glasses (
 id INT AUTO_INCREMENT PRIMARY KEY,
-brand VARCHAR(50) NOT NULL,
+id_brand INT NOT NULL,
 prescription_left FLOAT,
 prescription_right FLOAT,
 frame_type ENUM ("Rimless", "Plastic", "Metal"),
@@ -18,8 +27,8 @@ frame_color VARCHAR(20),
 left_glass_color VARCHAR(20),
 right_glass_color VARCHAR(20),
 price FLOAT,
-id_provider INT,
-FOREIGN KEY (id_provider) REFERENCES provider(id)
+FOREIGN KEY (id_brand) REFERENCES brand(id)
+ON UPDATE CASCADE ON DELETE RESTRICT
 );
 
 CREATE TABLE customer(
